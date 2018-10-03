65957ae6d071fe25a45c95001a62331cee7c1395
Ana Harris <vana1990@gmail.com>
2018-10-03 16:06:34 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -19,7 +19,16 @@ end
 #get_first_name_of_season_winner(data, "season 14")
 
 def get_contestant_name(data, occupation)
-  # code here
+  winners_name = ""
+  winner = nil
+  data[season].each do |arr_el|
+    # puts arr_el.inspect
+    if arr_el["status"] == "Winner"
+      winners_name = arr_el["name"]
+      winner = winners_name.split(" ").first
+    end
+  end
+  return winner
 end
 
 def count_contestants_by_hometown(data, hometown)
